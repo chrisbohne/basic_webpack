ba4d2188bcde7fd9b14f3cab1150451429d3dbac
chrisbohne <christoph.bohne90@gmail.com>
2022-02-15 10:44:08 +0100

    Add dev and prod config and scripts and

--- webpack.common.js
+++ webpack.common.js
@@ -2,12 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
-  output: {
-    filename: 'main.[contenthash].js', // adds hash to the name so every time it rebundles with changes it has a different name
-    path: path.resolve(__dirname, 'dist'),
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/template.html',
