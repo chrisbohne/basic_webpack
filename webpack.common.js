7f6109ca2c01c65b1e281b8b537659d2413e2431
chrisbohne <christoph.bohne90@gmail.com>
2022-02-15 12:29:30 +0100

    Add minifier for html and js on prod, and extract css into an extra file on prod

--- webpack.common.js
+++ webpack.common.js
@@ -1,22 +1,7 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = {
   entry: { main: './src/index.js', vendor: './src/vendor.js' },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/template.html',
-    }),
-  ],
   module: {
     rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader', // 3. injects styles into DOM
-          'css-loader', // 2. turns css into commonjs
-          'sass-loader', // 1. turns sass into css
-        ],
-      },
       {
         test: /\.html$/,
         use: ['html-loader'],
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,9 @@ const path = require('path');
 const common = require('./webpack.common');
 const { merge } = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -10,5 +13,33 @@ module.exports = merge(common, {
     path: path.resolve(__dirname, 'dist'),
     assetModuleFilename: 'imgs/[name].[hash][ext]',
   },
-  plugins: [new CleanWebpackPlugin()],
+  optimization: {
+    minimizer: [
+      new TerserPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/template.html',
+        minify: {
+          removeAttributeQuotes: true,
+          collapseWhitespace: true,
+          removeComments: true,
+        },
+      }),
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader, // 3. Extract css into files
+          'css-loader', // 2. turns css into commonjs
+          'sass-loader', // 1. turns sass into css
+        ],
+      },
+    ],
+  },
 });
